ccf86b0c3af05e08602888ccbfa3daaf5bacc1d7
rongbin.xie <rongbin.xie@voyageone.cn>
2019-04-01 18:59:22 +0800

    + change arc folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-ÿþ
+import logMessage from './js/logger'
+import './css/style.css'
+
+// Log message to console
+logMessage('Welcome to Expack!')
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ const nodeExternals = require('webpack-node-externals')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
   entry: {
-    server: './server.js',
+    server: './src/index.js',
   },
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/',
     filename: '[name].js'
   },
-  target: 'node',
+  target: 'web',  //Notice how we use target: ‘web’ for the app builds. This is VERY important, and you’ll get an error if you use target: ‘node’, so be sure to double check it.
   node: {
     // Need this when working with express, otherwise the build fails
     __dirname: false,   // if you don't put this is, __dirname
@@ -21,24 +21,34 @@ module.exports = {
   module: {
     rules: [
       {
-        // Transpiles ES6-8 into ES5
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: "babel-loader",
       },
       {
         // Loads the javacript into html template provided.
         // Entry point is set below in HtmlWebPackPlugin in Plugins
         test: /\.html$/,
-        use: [{loader: "html-loader"}]
+        use: [
+          {
+            loader: "html-loader",
+            //options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
       }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./index.html",
+      template: "./src/html/index.html",
       filename: "./index.html",
       /**
        * Note that excludeChunks will exclude a file called server which we don’t want to be included into our HTML file, since that is the webserver, and not needed in the app itself.
