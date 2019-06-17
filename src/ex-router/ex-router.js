4256513b4fabb0d7e8b29611fe8e5544faf73557
rongbin.xie <xrb16302@163.com>
2019-06-17 21:05:31 +0800

    + 修改express的业务

--- src/ex-router/ex-router.js
+++ src/ex-router/ex-router.js
@@ -5,52 +5,62 @@ let router = express.Router();
 module.exports = router;
 
 router.all((req, res, next) => {
-  console.log("request receive time: " + new Date().toDateString());
-  next();
+    console.log("request receive time: " + new Date().toDateString());
+    next();
 });
 
 router.get('/blog-list', (req, res) => {
-  BlogModel.find((err, blogs) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(blogs);
-    }
-  });
+    BlogModel.find((err, blogs) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(blogs);
+        }
+    });
 });
 
 router.get('/blog-view/:id', (req, res) => {
-  BlogModel.findById(req.params.id, (err, blog) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(blog);
-    }
-  })
+    BlogModel.findById(req.params.id, (err, blog) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(blog);
+        }
+    })
 });
 
 router.route('/blog-create').post((request, response) => {
-  console.log(request.body);
-  let blog = new BlogModel();
-  if (request.body) {
-    Object.assign(blog, request.body)
-  }
-  blog.save().then((blog) => {
-    console.log("----------> create successfully");
-    response.status(200).json(blog);
-  }).catch((err) => {
-    response.status(400).send("error when saving to MongoDB");
-  })
+    console.log(request.body);
+    let blog = new BlogModel();
+    if (request.body) {
+        Object.assign(blog, request.body)
+    }
+    blog.save().then((blog) => {
+        console.log("----------> create successfully");
+        response.status(200).json(blog);
+    }).catch((err) => {
+        response.status(400).send("error when saving to MongoDB");
+    })
 });
 //虽然用get可以，但是用delete可以吗？
 router.route("/blog-delete/:id").delete((req, res) => {
-  BlogModel.findByIdAndRemove({_id: req.params.id}, {useFindAndModify: true}, (err, todo) => {
-    console.log("delete %o", todo);
-    if (err) {
-      res.json(err);
-    } else {
-      res.json("delete successfully!");
-    }
-  })
+    BlogModel.findByIdAndRemove({_id: req.params.id}, {useFindAndModify: true}, (err, todo) => {
+        console.log("delete %o", todo);
+        if (err) {
+            res.json(err);
+        } else {
+            res.json("delete successfully!");
+        }
+    })
 });
 
+//更新博客
+router.route("/blog-update/:id").put((req, res) => {
+    BlogModel.findByIdAndUpdate(req.params.id, req.body, {useFindAndModify: true}, (err, todo) => {
+        if (err) {
+            res.json(err)
+        } else {
+            res.json(todo);
+        }
+    })
+});
