573de55363eafc96635253f0de430ff84c5b436a
rongbin.xie <xrb16302@163.com>
2019-06-18 09:07:55 +0800

    + 编写日志

--- src/components/blog-add.vue
+++ src/components/blog-add.vue
@@ -17,7 +17,7 @@
                 </td>
                 <td><label><---简单的分割线----></label></td>
                 <td>
-                    <p v-html="this.contextInHtml()" class="part"></p>
+                    <p v-html="convertMarkdown(blog.context)" class="part"></p>
                 </td>
             </tr>
         </table>
@@ -27,79 +27,81 @@
 </template>
 
 <script>
-  import {Converter} from 'showdown';
-  import {ApiService} from "../js/apiService";
-  import * as _ from "lodash";
+    import {Converter} from 'showdown';
+    import {ApiService} from "../js/apiService";
+    import * as _ from "lodash";
 
-  let converter = new Converter({tables: true});
-  let apiService = new ApiService();
+    let converter = new Converter({tables: true});
+    let apiService = new ApiService();
 
-  // converter.setFlavor('github');
-  let author = "asa-x";
+    // converter.setFlavor('github');
+    let author = "asa.x";
 
-  export default {
-    name: "blog-add",
-    data: function () {
-      return {
-        blog: {
-          title: "blog-title",
-          context: "hello,world",
-          id: null
-        },
-        contextInHtml: () => {
-          return converter.makeHtml(this.blog.context);
-        },
-      }
-    },
-    methods: {
-      create() {
-        apiService.createBlog(this.blog).then(res => {
-          if (!res._id) {
-            window.alert("res:" + res.message);
-          } else {
-            //页面跳转
-            this.$router.push({
-              path: '/blog-list',
-              name: 'BlogList'
-            })
-          }
-        })
-      },
-      update() {
-        let oldBlog = this.$route.params.blog;
-        if (oldBlog.title === this.blog.title
-          && oldBlog.context === this.blog.context
-          && oldBlog.author === this.blog.author) {
-          window.alert("there is no change!");
-          return;
-        }
-        apiService.update(this.blog).then((resp) => {
-          if (!resp) {
-            window.alert("update failed!");
-          }
-          this.$router.push({
-            path: '/blog-view',
-            name: 'BlogView',
-            params: {
-              blogId: oldBlog._id
+    export default {
+        name: "blog-add",
+        data: function () {
+            return {
+                blog: {
+                    title: "blog-title",
+                    context: "hello,world",
+                    author: author,
+                    id: null
+                },
             }
-          })
-        }, (error) => {
-          console.log(error);
-          window.alert("update failed!");
-        });
-      },
-      init() {
-        let vblog = this.$route.params.blog;
-        if (vblog) {
-          Object.assign(this.blog, vblog);
+        },
+        methods: {
+            create() {
+                apiService.createBlog(this.blog).then(res => {
+                    if (!res._id) {
+                        window.alert("res:" + res.message);
+                    } else {
+                        //页面跳转
+                        this.$router.push({
+                            path: '/blog-list',
+                            name: 'BlogList'
+                        })
+                    }
+                })
+            },
+            convertMarkdown(context) {
+                return converter.makeHtml(context);
+            },
+
+            init() {
+                let vblog = this.$route.params.blog;
+                if (vblog) {
+                    Object.assign(this.blog, vblog);
+                }
+            },
+            update() {
+                let oldBlog = this.$route.params.blog;
+                if (oldBlog.title === this.blog.title
+                    && oldBlog.context === this.blog.context
+                    && oldBlog.author === this.blog.author) {
+                    window.alert("there is no change!");
+                    return;
+                }
+                apiService.update(this.blog).then((resp) => {
+                    if (!resp) {
+                        window.alert("update failed!");
+                    }
+                    this.$router.push({
+                        path: '/blog-view',
+                        name: 'BlogView',
+                        params: {
+                            blogId: oldBlog._id
+                        }
+                    })
+                }, (error) => {
+                    console.log(error);
+                    window.alert("update failed!");
+                });
+            },
+        },
+        mounted() {
+            this.init()
         }
-      }
-    },
-    mounted() {
-      this.init()
     }
-  }
 </script>
 
 <style type="text/css" scoped>
--- src/components/blog-list.vue
+++ src/components/blog-list.vue
@@ -13,7 +13,7 @@
             <tbody>
             <tr v-for="blog in blogs" v-bind:key="blog.id">
                 <td>{{blog.title}}</td>
-                <td>{{blog.context}}</td>
+                <td v-html="convertMarkdown(blog.context)"></td>
                 <td>{{blog.author}}</td>
                 <td>{{blog.createTime}}</td>
                 <td>
@@ -28,65 +28,69 @@
 </template>
 
 <script>
-  //引入接口辅助类
-  import {ApiService} from '../js/apiService.js'
-  //定义一个对象
-  const apiService = new ApiService();
+    //引入接口辅助类
+    import {ApiService} from '../js/apiService.js'
+    //定义一个对象
+    const apiService = new ApiService();
+    import {Converter} from 'showdown';
 
-  // import {ApiService as apiService} from "../js/apiSerivce";
+    let converter = new Converter({tables: true});
+    // import {ApiService as apiService} from "../js/apiSerivce";
 
-  export default {
-    name: "blog-list",
-    data() {
-      return {
-        blogs: [],
-        numberOfBlogs: 0
-      }
-    },
-    //定义方法
-    methods: {
-      getBlogs() {
-        //调用接口获取数据，并且更新vue页面数据
-        apiService.getBlogs().then((resp) => {
-          console.log(resp);
-          this.blogs = resp;
-          this.numberOfBlogs = resp.length;
-        });
-      },
-      detail(blog) {
-        this.$router.push({
-          path: '/blog-view',
-          name: 'BlogView',
-          params: {
-            blogId: blog._id
-          }
-        })
-      },
-      del(blog) {
-        apiService.delBlog(blog._id).then(resp => {
-          if (resp) {
-            // window.alert(resp.data);
+    export default {
+        name: "blog-list",
+        data() {
+            return {
+                blogs: [],
+                numberOfBlogs: 0
+            }
+        },
+        //定义方法
+        methods: {
+            getBlogs() {
+                //调用接口获取数据，并且更新vue页面数据
+                apiService.getBlogs().then((resp) => {
+                    console.log(resp);
+                    this.blogs = resp;
+                    this.numberOfBlogs = resp.length;
+                });
+            },
+            convertMarkdown(context) {
+                return converter.makeHtml(context);
+            },
+            detail(blog) {
+                this.$router.push({
+                    path: '/blog-view',
+                    name: 'BlogView',
+                    params: {
+                        blogId: blog._id
+                    }
+                })
+            },
+            del(blog) {
+                apiService.delBlog(blog._id).then(resp => {
+                    if (resp) {
+                        this.getBlogs();
+                    } else {
+                        window.alert("blank resp");
+                    }
+                })
+            },
+            update(blog) {
+                this.$router.push({
+                    path: "/blog-add",
+                    name: "BlogAdd",
+                    params: {
+                        blog: blog
+                    }
+                })
+            }
+        },
+        //在vue被渲染的时候调用方法
+        mounted() {
             this.getBlogs();
-          } else {
-            window.alert("blank resp");
-          }
-        })
-      },
-      update(blog) {
-        this.$router.push({
-          path: "/blog-add",
-          name: "BlogAdd",
-          params: {
-            blog: blog
-          }
-        })
-      }
-    },
-    //在vue被渲染的时候调用方法
-    mounted() {
-      this.getBlogs();
+        }
     }
-  }
 </script>
 
 <style scoped>
