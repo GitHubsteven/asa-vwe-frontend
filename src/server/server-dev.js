3e6956baeb6b2d7669310dddfe7587f912fa7e91
rongbin.xie <rongbin.xie@voyageone.cn>
2020-04-01 19:05:36 +0800

    separate backend

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -3,13 +3,13 @@ import express from 'express'
 import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
-import config from '../../webpack.dev.config.js'
+import config from '../../webpack.server.config'
 
 let createError = require('http-errors');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-import router from '../ex-router/ex-router';
-import userRouter from '../ex-router/user-router';
+import router from '../router/router';
+import userRouter from '../router/user-router';
 
 //链接数据库
 let bodyParser = require('body-parser');
@@ -18,20 +18,18 @@ let mongoose = require('mongoose');
 let session = require('express-session');
 
 const app = express(),
-  DIST_DIR = __dirname,
-  HTML_FILE = path.join(DIST_DIR, 'index.html'),
   compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
+    publicPath: config.output.publicPath
 }));
 
 //服务器连接数据库
 let DB_URL = "mongodb://localhost:27017/blogs";
 mongoose.connect(DB_URL, {useNewUrlParser: true, useCreateIndex: true})
   .then(() => {
-      console.log("connect successfully!")
+        console.log("connect successfully!")
     }, (err) => {
-      console.log('error when connect to mongo db!' + err)
+        console.log('error when connect to mongo db!' + err)
     }
   );
 
@@ -46,32 +44,27 @@ app.use(cors());
 app.use(cookieParser());
 // 使用 session 中间件
 app.use(session({
-  secret :  'secret', // 对session id 相关的cookie 进行签名
-  resave : true,
-  saveUninitialized: false, // 是否保存未初始化的会话
-  cookie : {
-    maxAge : 1000 * 60 * 3, // 设置 session 的有效时间，单位毫秒
-  },
+    secret: 'secret', // 对session id 相关的cookie 进行签名
+    resave: true,
+    saveUninitialized: false, // 是否保存未初始化的会话
+    cookie: {
+        maxAge: 1000 * 60 * 3, // 设置 session 的有效时间，单位毫秒
+    },
 }));
 app.use(router);
 app.use(userRouter);
 
 app.use(webpackHotMiddleware(compiler, {
-  noInfo: false,
-  publicPath: config.output.publicPath
+    noInfo: false,
+    publicPath: config.output.publicPath
 }));
-
-app.use('/public', express.static('public'));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 const PORT = process.env.PORT || 8090;
 app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(`App listening to ${PORT}....`)
-  console.log('Press Ctrl+C to quit.')
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(`App listening to ${PORT}....`);
+    console.log('Press Ctrl+C to quit.')
 });
