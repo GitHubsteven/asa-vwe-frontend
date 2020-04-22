362238bdf3adf1729cc08814fe2ea10753bce1e7
rongbin.xie <rongbin.xie@voyageone.cn>
2020-04-22 15:46:34 +0800

    修改为前端工程

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,19 +1,27 @@
 /* eslint-disable no-undef */
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const {VueLoaderPlugin} = require('vue-loader')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const {VueLoaderPlugin} = require('vue-loader');
+let port = {
+    frontend: 9000,
+    api: 9090
+};
+
 
 module.exports = {
     entry: {
-        // main: './src/index.js'
-        main: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
-            './src/index.js']
+        main: './src/index.js'
+        // main: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+        //     './src/index.js']
     },
     output: {
         path: path.join(__dirname, 'dist'),
         publicPath: '/',
         filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.js', '.vue']
     },
     mode: 'development',
     target: 'web',
@@ -21,21 +29,26 @@ module.exports = {
     module: {
         rules: [
             {
-                enforce: "pre",
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-                options: {
-                    emitWarning: true,
-                    failOnError: false,
-                    failOnWarning: false
-                },
+                test: /\.vue?$/,
+                exclude: /(node_modules)/,
+                use: 'vue-loader'
             },
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
             },
+            // {
+            //     enforce: "pre",
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     loader: "eslint-loader",
+            //     options: {
+            //         emitWarning: true,
+            //         failOnError: false,
+            //         failOnWarning: false
+            //     },
+            // },
             {
                 // Loads the javacript into html template provided.
                 // Entry point is set below in HtmlWebPackPlugin in Plugins
@@ -56,13 +69,9 @@ module.exports = {
                 use: ['file-loader']
             },
             {
-                test: /\.vue$/,
-                use: ['vue-loader']
-            },
-          {
-            test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
-            loader: 'file-loader'
-          }
+                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                loader: 'file-loader'
+            }
             // ,
             // {
             //     test: /\.styl(us)?$/,
@@ -77,11 +86,23 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/html/index.html",
-            filename: "./index.html",
+            // filename: "./index.html",
             excludeChunks: ['server']
         }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
         new VueLoaderPlugin()
-    ]
-}
+    ],
+    devServer: {
+        historyApiFallback: true,
+        port: port.frontend,
+        contentBase: path.join(__dirname, "dist"),
+        proxy: {
+            '/api': {
+                target: 'http://localhost:' + port.api,
+                changeOrigin: true,
+                pathRewrite: {'/api': ''}
+            }
+        }
+    }
+};
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,73 +1,102 @@
-const path = require("path")
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+/* eslint-disable no-undef */
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const {VueLoaderPlugin} = require('vue-loader');
+const webpack = require('webpack');
 module.exports = {
-  entry: {
-    main: './src/index.js'
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
-  },
-  target: 'web',
-  devtool: 'source-map',
-  // Webpack 4 does not have a CSS minifier, although
-  // Webpack 5 will likely come with one
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true // set to true if you want JS source maps
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  },
-  module: {
-    rules: [
-      {
-        // Transpiles ES6-8 into ES5
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        // Loads the javacript into html template provided.
-        // Entry point is set below in HtmlWebPackPlugin in Plugins
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].js'
+    },
+    target: 'web',
+    devtool: 'source-map',
+    // Webpack 4 does not have a CSS minifier, although
+    // Webpack 5 will likely come with one
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
         ]
-      },
-      {
-        // Loads images into CSS and Javascript files
-        test: /\.jpg$/,
-        use: [{loader: "url-loader"}]
-      },
-      {
-        // Loads CSS into a file when you import it via Javascript
-        // Rules are set in MiniCssExtractPlugin
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue?$/,
+                exclude: /(node_modules)/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            },
+            // {
+            //     enforce: "pre",
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     loader: "eslint-loader",
+            //     options: {
+            //         emitWarning: true,
+            //         failOnError: false,
+            //         failOnWarning: false
+            //     },
+            // },
+            {
+                // Loads the javacript into html template provided.
+                // Entry point is set below in HtmlWebPackPlugin in Plugins
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        //options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                loader: 'file-loader'
+            }
+            // ,
+            // {
+            //     test: /\.styl(us)?$/,
+            //     use: [
+            //         'vue-style-loader',
+            //         'css-loader',
+            //         'stylus-loader'
+            //     ]
+            // }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/html/index.html",
+            // filename: "./index.html",
+            excludeChunks: ['server']
+        }),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/html/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
-}
+};
